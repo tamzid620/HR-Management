242754a688e444dbad2529d3416795cda8b0327e
git config --global user.name tamzid620 <tamzid620@gmail.com>
2024-01-17 11:44:03 +0600

    footer added

--- src/Components/Shared/Footer/Footer.jsx
+++ src/Components/Shared/Footer/Footer.jsx
@@ -1,11 +1,72 @@
-
+import footerimg from "../../../../public/images/footer_logo.png";
+import faceBookLogo from "../../../../public/icons/Facebook_Logo.png";
+import LinkdinLogo from "../../../../public/icons/Linkdin_logo.png";
+import YoutubeLogo from "../../../../public/icons/Youtube_logo.png";
 
 const Footer = () => {
-    return (
+  return (
+    <div>
+      <div
+        style={{ fontFamily: "Roboto, sans-serif" }}
+        className="bg-[#25476a] text-white grid lg:grid-cols-4 md:grid-cols-2 sm: grid-cols-1 gap-10 mx-auto p-8"
+      >
+        {/* footer div 1  */}
         <div>
-            
+          <img className="w-[100px]" src={footerimg} alt="" />
+          <p className="">
+            EDUMIG is a professional International education and immigration
+            consultant based in Dhaka & Chittagong, Bangladesh. We are the first
+            ICEF certified agent in Chittagong and one of the best education
+            service provider in town.
+          </p>
+          <p className="flex mt-5 gap-5">
+            <span>
+              <img
+                className="w-[30px] hover:shadow-xl hover:shadow-gray-600 hover:scale-[1.1]"
+                src={faceBookLogo}
+                alt=""
+              />
+            </span>
+            <span>
+              <img
+                className="w-[30px] hover:shadow-xl hover:shadow-gray-600 hover:scale-[1.1]"
+                src={LinkdinLogo}
+                alt=""
+              />
+            </span>
+            <span>
+              <img
+                className="w-[30px] hover:shadow-xl hover:shadow-gray-600 hover:scale-[1.1]"
+                src={YoutubeLogo}
+                alt=""
+              />
+            </span>
+          </p>
         </div>
-    );
+        {/* footer div 2  */}
+        <div>
+          <h1 className="font-bold text-xl mb-5">Useful Links</h1>
+          <p>ABOUT US</p>
+          <p>EVENT</p>
+          <p>OUR TEAM</p>
+          <p>STUDENT VISA</p>
+        </div>
+        {/* footer div 3  */}
+        <div>
+          <h1 className="font-bold text-xl mb-5">Location</h1>
+          <p>
+            House# 77, Flat # 9a, Road # 4, Block # C Banani, Dhaka
+            loremipsum.edu.com
+          </p>
+        </div>
+        {/* footer div 4  */}
+        <div>
+          <h1 className="font-bold text-xl mb-5">Hotline</h1>
+          <p>+8801569569523</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;

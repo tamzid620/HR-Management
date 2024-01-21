a19f6245fc1b7fb94782b06c7296dd79949893c9
git config --global user.name tamzid620 <tamzid620@gmail.com>
2024-01-21 12:48:43 +0600

    navbar and banner size fixed

--- src/Components/Shared/HomeSection/HomeBanner/HomeBanner.jsx
+++ src/Components/Shared/HomeSection/HomeBanner/HomeBanner.jsx
@@ -13,7 +13,7 @@ const HomeBanner = () => {
   };
 
   return (
-    <div className="pt-[100px]">
+    <div className="pt-[70px]">
       <div
         style={backgroundStyles}
         className="relative flex items-center justify-center min-h-screen"

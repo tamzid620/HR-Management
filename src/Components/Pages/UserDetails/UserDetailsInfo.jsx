5b27580e3e50cf123fc33fac6de4bf22d3f6954e
git config --global user.name tamzid620 <tamzid620@gmail.com>
2024-02-27 21:38:35 +0600

    user details info map removed

--- src/Components/Pages/UserDetails/UserDetailsInfo.jsx
+++ src/Components/Pages/UserDetails/UserDetailsInfo.jsx
@@ -5,8 +5,6 @@ import Swal from "sweetalert2";
 
 const UserDetailsInfo = () => {
   const [getToken, setGetToken] = useState([]);
-  const [eduInfo, setEduInfo] = useState([{ degreeName: "", institutionName: "", certificates: null, markSheet: null }]);
-  const [otherDocs, setOtherDocs] = useState([{documentsName:"", file:null}]);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -37,42 +35,6 @@ const UserDetailsInfo = () => {
       });
     }
   }, [navigate]);
-
-  const handleEducationalInfoChange = (index, key, event) => {
-    const newEducationalInfo = [...eduInfo];
-    newEducationalInfo[index][key] = event.target.value;
-    setEduInfo(newEducationalInfo);
-  };
-  
-  const handleEducationalInfoFileChange = (index, key, event) => {
-    const newEducationalInfo = [...eduInfo];
-    newEducationalInfo[index][key] = event.target.files[0];
-    setEduInfo(newEducationalInfo);
-  };
-  
-  const addEducationalInfo = () => {
-    setEduInfo([...eduInfo, { degreeName: "", institutionName: "", certificates: null, markSheet: null }]);
-  };
-  
-
-  // Function to handle changes in document name
-  const handleDocumentNameChange = (index, event) => {
-    const newDocuments = [...otherDocs];
-    newDocuments[index].name = event.target.value;
-    setOtherDocs(newDocuments);
-  };
-
-  // Function to handle file input changes
-  const handleFileInputChange = (index, event) => {
-    const newDocuments = [...otherDocs];
-    newDocuments[index].file = event.target.files[0];
-    setOtherDocs(newDocuments);
-  };
-
-  // Function to add a new document input
-  const addDocument = () => {
-    setOtherDocs([...otherDocs, { name: "", file: null }]);
-  };
 
   return (
     <div className="container mx-auto">
@@ -125,20 +87,17 @@ const UserDetailsInfo = () => {
       <div className="my-[50px]  leading-[50px] font-semibold ">
         <h1 className="text-3xl mb-[20px]">Educational Information</h1>
         {/* Add Educational button -------------- */}
-        <button  onClick={addEducationalInfo} className="btn btn-sm btn-success text-white ">
+        <button  className="btn btn-sm btn-success text-white ">
           Add Educational
         </button>
       <div>
 
-        {eduInfo.map((info, index) => (
-  <div key={index}>
+  <div>
     {/* Degree Name */}
     <div className="user_Details_paragraph">
       <label>Degree_Name:</label>
       <input
         type="text"
-        value={info.degreeName}
-        onChange={(event) => handleEducationalInfoChange(index, 'degreeName', event)}
         className="user_Details_span focus:outline-none focus:shadow-outline w-full bg-transparent  px-3 py-1 border-b-2  border-gray-600 "
       />
     </div>
@@ -147,17 +106,15 @@ const UserDetailsInfo = () => {
       <label>Institution_Name:</label>
       <input
         type="text"
-        value={info.institutionName}
-        onChange={(event) => handleEducationalInfoChange(index, 'institutionName', event)}
         className="user_Details_span focus:outline-none focus:shadow-outline w-full bg-transparent  px-3 py-1 border-b-2  border-gray-600 "
       />
     </div>
+    <section className="grid lg:grid-cols-2 md:grid-cols-2 sm: grid-cols-1">
     {/* Certificates */}
     <div className="user_Details_paragraph">
       <label>Certificates:</label>
       <input
         type="file"
-        onChange={(event) => handleEducationalInfoFileChange(index, 'certificates', event)}
         className="user_Details_span file-input  file-input-sm w-full max-w-x"
       />
     </div>
@@ -166,30 +123,27 @@ const UserDetailsInfo = () => {
       <label>MarkSheet:</label>
       <input
         type="file"
-        onChange={(event) => handleEducationalInfoFileChange(index, 'markSheet', event)}
         className="user_Details_span file-input  file-input-sm w-full max-w-x"
       />
     </div>
+    </section>
   </div>
-))}
 
       </div>
 {/* Other DOcuments div */}
         <h1 className="text-3xl my-[20px]">Other Documents</h1>
         <div>
         {/* Add Documents button -------------- */}
-        <button  onClick={addDocument} className="btn btn-sm btn-info text-white ">
+        <button className="btn btn-sm btn-info text-white ">
           Add Documents
         </button>
 
-        {otherDocs.map((otherDoc, index) => (
-            <div key={index} className="grid lg:grid-cols-2 md:grid-cols-2 sm: grid-cols-1">
+            <div className="grid lg:grid-cols-2 md:grid-cols-2 sm: grid-cols-1">
               <div className="user_Details_paragraph">
                 <label>Documents_Name:</label>
                 <input
                   type="text"
                   name="documentName"
-                  onChange={(event) => handleDocumentNameChange(index, event)}
                   className="user_Details_span focus:outline-none focus:shadow-outline w-full bg-transparent  px-3 py-1 border-b-2  border-gray-600 "
                 />
               </div>
@@ -198,12 +152,10 @@ const UserDetailsInfo = () => {
                 <input
                   type="file"
                   name= "documentFile"
-                  onChange={(event) => handleFileInputChange(index, event)}
                   className="user_Details_span file-input  file-input-sm w-full max-w-x"
                 />
               </div>
             </div>
-          ))}
         </div>
       </div>
     </div>
